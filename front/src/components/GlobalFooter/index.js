e9e408fc5e3b85a0919a13f9081f450dc234594a
koori0829 <1079414416@qq.com>
2019-03-14 19:05:40 +0800

    components of ant design pro

---
+++ front/src/components/GlobalFooter/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import classNames from 'classnames';
+import styles from './index.less';
+import settings from '../../../settings';
+import { FormattedMessage } from 'umi/locale';
+
+const GlobalFooter = ({ className, links, copyright }) => {
+  const clsString = classNames(styles.globalFooter, className);
+  return (
+    <footer className={clsString}>
+      {/* {links && (
+        <div className={styles.links}>
+          {links.map(link => (
+            <a
+              key={link.key}
+              title={link.key}
+              target={link.blankTarget ? '_blank' : '_self'}
+              href={link.href}
+            >
+              {link.title}
+            </a>
+          ))}
+        </div>
+      )} */}
+      {/* {copyright && <div className={styles.copyright}>{copyright}</div>} */}
+      <p><FormattedMessage id="app.version" />{settings.version}</p>
+    </footer>
+  );
+};
+
+export default GlobalFooter;
