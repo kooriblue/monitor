3fb33dcc82255de2ae84179042c8a0a977010cce
koori0829 <1079414416@qq.com>
2019-03-14 19:02:24 +0800

    constants used by project

---
+++ front/settings.js
@@ -0,0 +1,8 @@
+//use server as proxy
+//server's url
+module.exports = {
+    requestUrl: "http://127.0.0.1:3001/", //server的地址
+    version: "1.0", //版本号
+    timeRange: 600, //登录时限，秒为单位
+    passwordSalt: "BFD8363B6C74EA86", //密码盐值
+}
