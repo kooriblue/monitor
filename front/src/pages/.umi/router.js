320ddd0c19a11f47115a9f0c34dd24912fd9ec41
koori0829 <1079414416@qq.com>
2019-03-15 23:41:18 +0800

    change pages settings

--- front/src/pages/.umi/router.js
+++ front/src/pages/.umi/router.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Router as DefaultRouter, Route, Switch } from 'react-router-dom';
 import dynamic from 'umi/dynamic';
 import renderRoutes from 'umi/_renderRoutes';
-import RendererWrapper0 from 'C:/koori/learning/webprojects/antd/front/src/pages/.umi/LocaleWrapper.jsx'
+import RendererWrapper0 from 'C:/koori/github/monitor/monitor/front/src/pages/.umi/LocaleWrapper.jsx'
 import _dvaDynamic from 'dva/dynamic'
 
 let Router = require('dva/router').routerRedux.ConnectedRouter;
@@ -13,7 +13,7 @@ let routes = [
     "component": _dvaDynamic({
   
   component: () => import(/* webpackChunkName: "layouts__UserLayout" */'../../layouts/UserLayout'),
-  LoadingComponent: require('C:/koori/learning/webprojects/antd/front/src/components/PageLoading/index').default,
+  LoadingComponent: require('C:/koori/github/monitor/monitor/front/src/components/PageLoading/index').default,
 }),
     "routes": [
       {
@@ -26,39 +26,15 @@ let routes = [
         "component": _dvaDynamic({
   app: window.g_app,
 models: () => [
-  import(/* webpackChunkName: 'p__User__models__register.js' */'C:/koori/learning/webprojects/antd/front/src/pages/User/models/register.js').then(m => { return { namespace: 'register',...m.default}})
+  import(/* webpackChunkName: 'p__User__models__register.js' */'C:/koori/github/monitor/monitor/front/src/pages/User/models/register.js').then(m => { return { namespace: 'register',...m.default}})
 ],
   component: () => import(/* webpackChunkName: "p__User__Login" */'../User/Login'),
-  LoadingComponent: require('C:/koori/learning/webprojects/antd/front/src/components/PageLoading/index').default,
+  LoadingComponent: require('C:/koori/github/monitor/monitor/front/src/components/PageLoading/index').default,
 }),
         "exact": true
       },
       {
-        "path": "/user/register",
-        "component": _dvaDynamic({
-  app: window.g_app,
-models: () => [
-  import(/* webpackChunkName: 'p__User__models__register.js' */'C:/koori/learning/webprojects/antd/front/src/pages/User/models/register.js').then(m => { return { namespace: 'register',...m.default}})
-],
-  component: () => import(/* webpackChunkName: "p__User__Register" */'../User/Register'),
-  LoadingComponent: require('C:/koori/learning/webprojects/antd/front/src/components/PageLoading/index').default,
-}),
-        "exact": true
-      },
-      {
-        "path": "/user/register-result",
-        "component": _dvaDynamic({
-  app: window.g_app,
-models: () => [
-  import(/* webpackChunkName: 'p__User__models__register.js' */'C:/koori/learning/webprojects/antd/front/src/pages/User/models/register.js').then(m => { return { namespace: 'register',...m.default}})
-],
-  component: () => import(/* webpackChunkName: "p__User__RegisterResult" */'../User/RegisterResult'),
-  LoadingComponent: require('C:/koori/learning/webprojects/antd/front/src/components/PageLoading/index').default,
-}),
-        "exact": true
-      },
-      {
-        "component": () => React.createElement(require('C:/koori/learning/webprojects/antd/front/node_modules/umi-build-dev/lib/plugins/404/NotFound.js').default, { pagesPath: 'src/pages', hasRoutesInConfig: true })
+        "component": () => React.createElement(require('C:/koori/github/monitor/monitor/front/node_modules/umi-build-dev/lib/plugins/404/NotFound.js').default, { pagesPath: 'src/pages', hasRoutesInConfig: true })
       }
     ]
   },
@@ -67,7 +43,7 @@ models: () => [
     "component": _dvaDynamic({
   
   component: () => import(/* webpackChunkName: "layouts__BasicLayout" */'../../layouts/BasicLayout'),
-  LoadingComponent: require('C:/koori/learning/webprojects/antd/front/src/components/PageLoading/index').default,
+  LoadingComponent: require('C:/koori/github/monitor/monitor/front/src/components/PageLoading/index').default,
 }),
     "Routes": [require('../Authorized').default],
     "authority": [
@@ -90,7 +66,7 @@ models: () => [
             "component": _dvaDynamic({
   
   component: () => import(/* webpackChunkName: "layouts__BasicLayout" */'../PayOrder/QueryOrder'),
-  LoadingComponent: require('C:/koori/learning/webprojects/antd/front/src/components/PageLoading/index').default,
+  LoadingComponent: require('C:/koori/github/monitor/monitor/front/src/components/PageLoading/index').default,
 }),
             "exact": true
           },
@@ -100,7 +76,7 @@ models: () => [
             "component": _dvaDynamic({
   
   component: () => import(/* webpackChunkName: "layouts__BasicLayout" */'../PayOrder/OrderStatistics'),
-  LoadingComponent: require('C:/koori/learning/webprojects/antd/front/src/components/PageLoading/index').default,
+  LoadingComponent: require('C:/koori/github/monitor/monitor/front/src/components/PageLoading/index').default,
 }),
             "exact": true
           },
@@ -110,12 +86,12 @@ models: () => [
             "component": _dvaDynamic({
   
   component: () => import(/* webpackChunkName: "layouts__BasicLayout" */'../PayOrder/RefundList'),
-  LoadingComponent: require('C:/koori/learning/webprojects/antd/front/src/components/PageLoading/index').default,
+  LoadingComponent: require('C:/koori/github/monitor/monitor/front/src/components/PageLoading/index').default,
 }),
             "exact": true
           },
           {
-            "component": () => React.createElement(require('C:/koori/learning/webprojects/antd/front/node_modules/umi-build-dev/lib/plugins/404/NotFound.js').default, { pagesPath: 'src/pages', hasRoutesInConfig: true })
+            "component": () => React.createElement(require('C:/koori/github/monitor/monitor/front/node_modules/umi-build-dev/lib/plugins/404/NotFound.js').default, { pagesPath: 'src/pages', hasRoutesInConfig: true })
           }
         ]
       },
@@ -130,12 +106,12 @@ models: () => [
             "component": _dvaDynamic({
   
   component: () => import(/* webpackChunkName: "layouts__BasicLayout" */'../Device/QueryDevice'),
-  LoadingComponent: require('C:/koori/learning/webprojects/antd/front/src/components/PageLoading/index').default,
+  LoadingComponent: require('C:/koori/github/monitor/monitor/front/src/components/PageLoading/index').default,
 }),
             "exact": true
           },
           {
-            "component": () => React.createElement(require('C:/koori/learning/webprojects/antd/front/node_modules/umi-build-dev/lib/plugins/404/NotFound.js').default, { pagesPath: 'src/pages', hasRoutesInConfig: true })
+            "component": () => React.createElement(require('C:/koori/github/monitor/monitor/front/node_modules/umi-build-dev/lib/plugins/404/NotFound.js').default, { pagesPath: 'src/pages', hasRoutesInConfig: true })
           }
         ]
       },
@@ -150,7 +126,7 @@ models: () => [
             "component": _dvaDynamic({
   
   component: () => import(/* webpackChunkName: "layouts__BasicLayout" */'../Member/QueryMember'),
-  LoadingComponent: require('C:/koori/learning/webprojects/antd/front/src/components/PageLoading/index').default,
+  LoadingComponent: require('C:/koori/github/monitor/monitor/front/src/components/PageLoading/index').default,
 }),
             "exact": true
           },
@@ -160,7 +136,7 @@ models: () => [
             "component": _dvaDynamic({
   
   component: () => import(/* webpackChunkName: "layouts__BasicLayout" */'../Member/AddMember'),
-  LoadingComponent: require('C:/koori/learning/webprojects/antd/front/src/components/PageLoading/index').default,
+  LoadingComponent: require('C:/koori/github/monitor/monitor/front/src/components/PageLoading/index').default,
 }),
             "exact": true
           },
@@ -170,12 +146,12 @@ models: () => [
             "component": _dvaDynamic({
   
   component: () => import(/* webpackChunkName: "layouts__BasicLayout" */'../Member/TransferLicense'),
-  LoadingComponent: require('C:/koori/learning/webprojects/antd/front/src/components/PageLoading/index').default,
+  LoadingComponent: require('C:/koori/github/monitor/monitor/front/src/components/PageLoading/index').default,
 }),
             "exact": true
           },
           {
-            "component": () => React.createElement(require('C:/koori/learning/webprojects/antd/front/node_modules/umi-build-dev/lib/plugins/404/NotFound.js').default, { pagesPath: 'src/pages', hasRoutesInConfig: true })
+            "component": () => React.createElement(require('C:/koori/github/monitor/monitor/front/node_modules/umi-build-dev/lib/plugins/404/NotFound.js').default, { pagesPath: 'src/pages', hasRoutesInConfig: true })
           }
         ]
       },
@@ -190,48 +166,12 @@ models: () => [
             "component": _dvaDynamic({
   
   component: () => import(/* webpackChunkName: "layouts__BasicLayout" */'../Application/ProductManagement'),
-  LoadingComponent: require('C:/koori/learning/webprojects/antd/front/src/components/PageLoading/index').default,
+  LoadingComponent: require('C:/koori/github/monitor/monitor/front/src/components/PageLoading/index').default,
 }),
             "exact": true
           },
           {
-            "component": () => React.createElement(require('C:/koori/learning/webprojects/antd/front/node_modules/umi-build-dev/lib/plugins/404/NotFound.js').default, { pagesPath: 'src/pages', hasRoutesInConfig: true })
-          }
-        ]
-      },
-      {
-        "path": "/activationcode",
-        "name": "activationcode",
-        "icon": "profile",
-        "routes": [
-          {
-            "path": "/activationcode/generate",
-            "name": "activationcode.generate",
-            "component": _dvaDynamic({
-  app: window.g_app,
-models: () => [
-  import(/* webpackChunkName: 'p__ActivationCode__models__activationcode.js' */'C:/koori/learning/webprojects/antd/front/src/pages/ActivationCode/models/activationcode.js').then(m => { return { namespace: 'activationcode',...m.default}})
-],
-  component: () => import(/* webpackChunkName: "layouts__BasicLayout" */'../ActivationCode/ActivationCodeForm'),
-  LoadingComponent: require('C:/koori/learning/webprojects/antd/front/src/components/PageLoading/index').default,
-}),
-            "exact": true
-          },
-          {
-            "path": "/activationcode/show",
-            "name": "activationcode.show",
-            "component": _dvaDynamic({
-  app: window.g_app,
-models: () => [
-  import(/* webpackChunkName: 'p__ActivationCode__models__activationcode.js' */'C:/koori/learning/webprojects/antd/front/src/pages/ActivationCode/models/activationcode.js').then(m => { return { namespace: 'activationcode',...m.default}})
-],
-  component: () => import(/* webpackChunkName: "layouts__BasicLayout" */'../ActivationCode/ShowActivationCode'),
-  LoadingComponent: require('C:/koori/learning/webprojects/antd/front/src/components/PageLoading/index').default,
-}),
-            "exact": true
-          },
-          {
-            "component": () => React.createElement(require('C:/koori/learning/webprojects/antd/front/node_modules/umi-build-dev/lib/plugins/404/NotFound.js').default, { pagesPath: 'src/pages', hasRoutesInConfig: true })
+            "component": () => React.createElement(require('C:/koori/github/monitor/monitor/front/node_modules/umi-build-dev/lib/plugins/404/NotFound.js').default, { pagesPath: 'src/pages', hasRoutesInConfig: true })
           }
         ]
       },
@@ -246,40 +186,30 @@ models: () => [
             "component": _dvaDynamic({
   
   component: () => import(/* webpackChunkName: "layouts__BasicLayout" */'../Account/ChangePassword'),
-  LoadingComponent: require('C:/koori/learning/webprojects/antd/front/src/components/PageLoading/index').default,
+  LoadingComponent: require('C:/koori/github/monitor/monitor/front/src/components/PageLoading/index').default,
 }),
             "exact": true
           },
           {
-            "component": () => React.createElement(require('C:/koori/learning/webprojects/antd/front/node_modules/umi-build-dev/lib/plugins/404/NotFound.js').default, { pagesPath: 'src/pages', hasRoutesInConfig: true })
+            "component": () => React.createElement(require('C:/koori/github/monitor/monitor/front/node_modules/umi-build-dev/lib/plugins/404/NotFound.js').default, { pagesPath: 'src/pages', hasRoutesInConfig: true })
           }
         ]
-      },
-      {
-        "path": "/payment",
-        "name": "payment",
-        "component": _dvaDynamic({
-  
-  component: () => import(/* webpackChunkName: "p__Braintree__PayPal" */'../Braintree/PayPal.js'),
-  LoadingComponent: require('C:/koori/learning/webprojects/antd/front/src/components/PageLoading/index').default,
-}),
-        "exact": true
       },
       {
         "component": _dvaDynamic({
   
   component: () => import(/* webpackChunkName: "p__404" */'../404'),
-  LoadingComponent: require('C:/koori/learning/webprojects/antd/front/src/components/PageLoading/index').default,
+  LoadingComponent: require('C:/koori/github/monitor/monitor/front/src/components/PageLoading/index').default,
 }),
         "exact": true
       },
       {
-        "component": () => React.createElement(require('C:/koori/learning/webprojects/antd/front/node_modules/umi-build-dev/lib/plugins/404/NotFound.js').default, { pagesPath: 'src/pages', hasRoutesInConfig: true })
+        "component": () => React.createElement(require('C:/koori/github/monitor/monitor/front/node_modules/umi-build-dev/lib/plugins/404/NotFound.js').default, { pagesPath: 'src/pages', hasRoutesInConfig: true })
       }
     ]
   },
   {
-    "component": () => React.createElement(require('C:/koori/learning/webprojects/antd/front/node_modules/umi-build-dev/lib/plugins/404/NotFound.js').default, { pagesPath: 'src/pages', hasRoutesInConfig: true })
+    "component": () => React.createElement(require('C:/koori/github/monitor/monitor/front/node_modules/umi-build-dev/lib/plugins/404/NotFound.js').default, { pagesPath: 'src/pages', hasRoutesInConfig: true })
   }
 ];
 window.g_routes = routes;
